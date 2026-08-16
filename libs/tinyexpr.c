95d036ee515026f459ac5fb25a40ad95bc275752
var4pro <var4pro@gmail.com>
2026-08-16 03:12:08 +0300

    minor change

--- libs/tinyexpr.c
+++ libs/tinyexpr.c
@@ -35,7 +35,7 @@ For log = base 10 log do nothing
 For log = natural log uncomment the next line. */
 /* #define TE_NAT_LOG */
 
-#include "include/tinyexpr.h"
+#include "tinyexpr.h"
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
